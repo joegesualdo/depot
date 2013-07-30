573fbff7d747927abafc52606f08a666cda65cbc
Joe Gesualdo <joegesualdo@gmail.com>
2013-07-30 09:08:49 -0400

    Add total cost to cart view

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,8 +1,21 @@
+# == Schema Information
+#
+# Table name: carts
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Cart < ActiveRecord::Base
   #attr_accessible :title, :body
 
  has_many :line_items, dependent: :destroy
-  
+
+ def total_price
+   line_items.to_a.sum {|item| item.total_price}
+ end
+
  def add_product(product_id)
    current_item = line_items.find_by_product_id(product_id)
    if current_item
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,7 +1,22 @@
+# == Schema Information
+#
+# Table name: line_items
+#
+#  id         :integer          not null, primary key
+#  product_id :integer
+#  cart_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  quantity   :integer          default(1)
+#
+
 class LineItem < ActiveRecord::Base
   attr_accessible :cart_id, :product_id
 
   belongs_to :cart
   belongs_to :product
 
+  def total_price
+    product.price * quantity
+  end
 end
